f2e32d3df1294c744982ba0dec8c3dd85b2b9188
jhonas-cloud <mr.jhonascabanero@gmail.com>
2025-04-10 15:16:51 +0000

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-sudo yum install tomcat -y
+sudo yum -y install tomcat 
 sudo yum -y install httpd
-sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+
+sudo tee > /etc/httpd/conf.d/tomcat_manager.conf > /dev/null << EOF
 <VirtualHost *:80>
   ServerAdmin root@localhost
   ServerName app.nextwork.com
+
   DefaultType text/html
   ProxyRequests off
   ProxyPreserveHost On
+
   ProxyPass / http://localhost:8080/nextwork-web-project/
   ProxyPassReverse / http://localhost:8080/nextwork-web-project/
 </VirtualHost>
